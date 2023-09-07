6fe9df3d5822cf70b94a37c516354942ebe5eb54
khizarongit <85388241+khizarongit@users.noreply.github.com>
2023-09-07 06:27:03 +0000

    Enhanced The WebApp

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-let url = url("https://api.currencyapi.com/v3/latest?apikey=cur_live_7teISEWHy2GiS6GUYnfB3M2bye52NVdnzZpwG2jQ")
+// let url = url("https://api.currencyapi.com/v3/latest?apikey=cur_live_7teISEWHy2GiS6GUYnfB3M2bye52NVdnzZpwG2jQ")
+let btn = document.getElementsByClassName("btn")
+btn.addEventListener('click' , () => {
+    alert("Button")
+})
